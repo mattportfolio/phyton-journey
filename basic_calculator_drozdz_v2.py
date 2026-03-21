989209ce85684ad8606509c66a5e026a1ee76ecc
Mateusz Dróżdż <mattsportfolio328@gmail.com>
2026-03-21 16:54:47 +0100

    Refactor calculator error messages and prompts
    Small changes in the script

--- basic_calculator_drozdz_v2.py
+++ basic_calculator_drozdz_v2.py
@@ -3,37 +3,34 @@
 def add(a, b):
     return a + b
 def subtract(a, b):
-    return a - b 
+    return a - b
 def multiply(a, b):
     return a * b
 def divide(a, b):
-    return a / b
     if b == 0:
-        return("Error: You cannot divide by zero!")
-        
-#1 Ask for numbers
+        return "Error: We dont divide by zero lad!"
+    return a / b
+    
+# Get their numbers
 
-n1 = int(input("Enter the 1st nb:  "))
-n2 = int(input("Enter the 2nd nb:  "))
+n1 = int(input("Enter 1st number:  "))
+n2 = int(input("Enter 2nd number:  "))
 
-# Ask what they want to do with it
+# What to do with them
 
 print("\nChoose an operation")
 print("1. Add (+)")
 print("2. Subtract (-)")
 print("3. Multiply (*)")
 print("4. Divide (/)")
-
-choice = input("Enter choice (1/2/3/4): ")
-
-# Logistics
+choice = input("(1/2/3/4):  ")
 
 if choice == '1':
     print(f"Result: {n1} + {n2} = {add(n1, n2)}")
-elif choice == '2':
+if choice == '2':
     print(f"Result: {n1} - {n2} = {subtract(n1, n2)}")
 elif choice == '3':
     print(f"Result: {n1} * {n2} = {multiply(n1, n2)}")
-elif choice == '4':
-    print(f"Result: {n1} * {n2} = {divide(n1, n2)}")
-
+if choice == '4':
+    print(f"Result: {n1} / {n2} = {divide(n1, n2)}")
+    
