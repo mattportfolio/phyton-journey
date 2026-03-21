d6b3b47903e27476db9515aa6114b5f9caa08f31
Mateusz Dróżdż <mattsportfolio328@gmail.com>
2026-03-21 15:50:30 +0100

    Add files via upload

---
+++ basic_calculator_drozdz_v2.py
@@ -0,0 +1,39 @@
+#Mateusz Drozdz
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b 
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+    if b == 0:
+        return("Error: You cannot divide by zero!")
+        
+#1 Ask for numbers
+
+n1 = int(input("Enter the 1st nb:  "))
+n2 = int(input("Enter the 2nd nb:  "))
+
+# Ask what they want to do with it
+
+print("\nChoose an operation")
+print("1. Add (+)")
+print("2. Subtract (-)")
+print("3. Multiply (*)")
+print("4. Divide (/)")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+# Logistics
+
+if choice == '1':
+    print(f"Result: {n1} + {n2} = {add(n1, n2)}")
+elif choice == '2':
+    print(f"Result: {n1} - {n2} = {subtract(n1, n2)}")
+elif choice == '3':
+    print(f"Result: {n1} * {n2} = {multiply(n1, n2)}")
+elif choice == '4':
+    print(f"Result: {n1} * {n2} = {divide(n1, n2)}")
+
